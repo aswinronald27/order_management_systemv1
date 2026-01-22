07e0ebce1a071dd61c3c3d2a564e74fd630d1631
Aswin Ronald <aswinronald27@gmail.com>
2026-01-22 23:47:09 +0530

    Fix ESLint unused imports for Vercel build

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Usermain from "../Sections/User Section/UserMainSection/Usermain";
 import UserOrderSection from "../Sections/User Section/UserTabSection/UserOrderSection";
 const UserDashboard = () => {
